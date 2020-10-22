5969734622fdcdc8ca35f57c75812d4e43ca0a8f
tuckers18 <506932@chrome.escg.ac.uk>
2020-10-22 13:38:12 +0100

    no message

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool reeceIsThin = false;
+    void Start()
+    {
+       Debug.Log("hi jack"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+} 
